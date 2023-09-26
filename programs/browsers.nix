b3eb246e681e3d683ed7e79183643bc1ed25a210
Mihran Mashhud <mihranmashhud@gmail.com>
2023-09-26 07:58:09 -0400

    Fix formatting

--- programs/browsers.nix
+++ programs/browsers.nix
@@ -34,7 +34,7 @@
             urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
             iconUpdateURL = "https://nixos.wiki/favicon.png";
             updateInterval = 24 * 60 * 60 * 1000; # every day
-              definedAliases = [ "nw" ];
+            definedAliases = [ "nw" ];
           };
           "Bing".metaData.hidden = "true";
         };
