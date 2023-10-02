7467dae4c7158bfc0298f0e7e83684ce459879cf
Mihran Mashhud <mihranmashhud@gmail.com>
2023-10-02 05:09:33 -0400

    use bluez-experimental and change audio codecs

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -189,8 +189,21 @@
   };
 
   # Bluetooth
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    package = pkgs.bluez5-experimental;
+  };
   services.blueman.enable = true;
+  environment.etc = {
+    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+      bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+      }
+    '';
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mihranmashhud = {
