e41f5797e36553b95d4add92ee7f6241d68c02c7
Mihran Mashhud <mihranmashhud@gmail.com>
2023-10-15 21:31:08 -0400

    Remove grimblast.
    It is included in nixpkgs now.

--- derivations/flake.nix
+++ derivations/flake.nix
@@ -7,10 +7,6 @@
       url = "git+https://invent.kde.org/system/xwaylandvideobridge.git";
       flake = false;
     };
-    grimblast = {
-      url = "github:hyprwm/contrib?dir=grimblast";
-      flake = false;
-    };
   };
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
@@ -22,7 +18,6 @@
       {
         packages = with pkgs; {
           xwaylandvideobridge = libsForQt5.callPackage ./xwaylandvideobridge.nix { inherit inputs; };
-          grimblast = callPackage ./grimblast.nix { inherit inputs; };
           alvr = callPackage ./alvr-appimage.nix {};
         };
       });
