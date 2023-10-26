d0cd1759fb3b861568fd8544b354cd55bd162b97
Mihran Mashhud <mihranmashhud@gmail.com>
2023-10-25 20:42:53 -0400

    Remove slack overrides.

--- home.nix
+++ home.nix
@@ -27,18 +27,6 @@
 
 
   home.packages =
-    let
-      slack = pkgs.slack.overrideAttrs (old: {
-        installPhase = old.installPhase + ''
-          rm $out/bin/slack
-
-          makeWrapper $out/lib/slack/slack $out/bin/slack \
-            --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
-            --prefix PATH : ${lib.makeBinPath [pkgs.xdg-utils]} \
-            --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
-        '';
-      });
-    in
     with (pkgs); [
       # apps
       gnome.nautilus
