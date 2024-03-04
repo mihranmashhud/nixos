7c69d7d9e839bc0114707b45bc7311172b3cb5fe
Mihran Mashhud <mihranmashhud@gmail.com>
2024-03-04 16:36:56 -0500

    Fix deprecation warnings. Remove some comments.

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -148,9 +148,9 @@
   services.automatic-timezoned.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Printing
@@ -326,13 +326,6 @@
     };
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
   programs = {
     zsh.enable = true;
     hyprland.enable = true;
