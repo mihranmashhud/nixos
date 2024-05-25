352d6c223158e83836d9a2f6039079b3a267c7f0
Mihran Mashhud <mihranmashhud@gmail.com>
2024-05-24 22:15:54 -0400

    Dualsense controller udev rules.

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -349,6 +349,13 @@
     qmk-udev-rules
     vial-udev-rules
   ];
+  services.udev.extraRules = ''
+    # PS5 DualSense controller over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ce6", MODE="0660", TAG+="uaccess"
+
+    # PS5 DualSense controller over bluetooth hidraw
+    KERNEL=="hidraw*", KERNELS=="*054C:0CE6*", MODE="0660", TAG+="uaccess"
+  '';
 
   services.postgresql = {
     enable = true;
