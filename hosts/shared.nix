c2d12aa4070178cde6c54d75a535378ea241c26f
Mihran Mashhud <mihranmashhud@gmail.com>
2024-05-24 22:14:34 -0400

    Remove nix settings (these are now set in the flake)

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -384,14 +384,6 @@
 
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
-    auto-optimise-store = true;
-    substituters = [
-      "https://nix-community.cachix.org"
-      "https://cache.nixos.org/"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
   };
 
   programs.nh = {
