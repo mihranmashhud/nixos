e5df24a932da1689117add972c50db41142c6250
Mihran Mashhud <mihranmashhud@gmail.com>
2024-05-24 21:56:32 -0400

    Update wlxoverlay.nix

--- derivations/wlxoverlay.nix
+++ derivations/wlxoverlay.nix
@@ -5,8 +5,8 @@
 appimageTools.wrapType2 {
   name = "wlxoverlay";
   src = fetchurl {
-    url = "https://github.com/galister/WlxOverlay/releases/download/v1.4.1/WlxOverlay-v1.4.1-x86_64.AppImage";
-    hash = "sha256-/NXI/6c4lZxgFXx+/y3UQPa14rNI1pAvkBTQilO/mlE=";
+    url = "https://github.com/galister/WlxOverlay/releases/download/v1.4.5/WlxOverlay-v1.4.5-x86_64.AppImage";
+    hash = "sha256-sQfAqxpxVVC2JpPdK0Fcy5UHGnNWG2rJAgBrk2oWnN4=";
   };
   extraPkgs = pkgs:
     with pkgs; [
