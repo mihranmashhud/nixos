dc635c01af317845b530eb7267c5c440464903cc
mihranmashhud <mihranmashhud@gmail.com>
2024-07-18 17:51:48 -0400

    Disable dualsense touchpad.

--- packages/dualsense-udev-rules/default.nix
+++ packages/dualsense-udev-rules/default.nix
@@ -7,6 +7,9 @@ writeTextFile {
 
     # PS5 DualSense controller over bluetooth hidraw
     KERNEL=="hidraw*", KERNELS=="*054C:0CE6*", MODE="0660", TAG+="uaccess"
+
+    # Disable DualSense touchpad
+    ACTION=="add|change", KERNEL=="event[0-9]*", ATTRS{name}=="*Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
   '';
   destination = "/etc/udev/rules.d/99-dualsense.rules";
 }
