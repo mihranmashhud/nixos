142825a5150670c0339d3a767f05f0bb3595cf55
mihranmashhud <mihranmashhud@gmail.com>
2024-07-18 20:49:14 -0400

    Update server config.

--- systems/x86_64-linux/mihranServer/default.nix
+++ systems/x86_64-linux/mihranServer/default.nix
@@ -9,7 +9,7 @@
 with lib;
 with lib.internal; {
   imports = [
-    # ./hardware.nix # Make sure to add the generated hardware config.
+    ./hardware.nix # Make sure to add the generated hardware config.
   ];
   internal = {
     locale = enabled;
@@ -20,4 +20,12 @@ with lib.internal; {
       ssh = enabled;
     };
   };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
