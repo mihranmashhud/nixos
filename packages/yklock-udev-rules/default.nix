d42e5086ed2467f0d68a0e34ad5ab62daa63ab01
mihranmashhud <mihranmashhud@gmail.com>
2024-10-13 16:41:48 -0400

    Properly working yubikey lockscreen udev rules

--- packages/yklock-udev-rules/default.nix
+++ packages/yklock-udev-rules/default.nix
@@ -1,38 +1,24 @@
 {
+  systemd,
+  stdenv,
   writeTextFile,
-  writeShellApplication,
-  pamtester,
-  procps,
-  hyprlock, libnotify,
 }: let
-  yklock-pkg = writeShellApplication {
-    name = "yklock";
-    runtimeInputs = [pamtester hyprlock procps libnotify];
-    text =
-      /*
-      bash
-      */
-      ''
-        if [[ "$1" == "lock" ]]; then
-          notify-send "Yubikey unplugged." 
-          pidof hyprlock || hyprlock
-        elif [[ "$1" == "unlock" ]]; then
-          if echo "" | pamtester login "$USER" authenticate; then
-            # PAM login successful
-            # kill locker
-            notify-send "Yubikey plugged in." 
-            pkill -SIGUSR1 hyprlock
-          fi
-        fi
-        exit 0
-      '';
+  udevRules = writeTextFile {
+    name = "yklock-udev-rules";
+    text = ''
+      ACTION=="remove",\
+      ENV{ID_VENDOR}=="Yubico",\
+      ENV{ID_VENDOR_ID}=="1050",\
+      ENV{ID_MODEL_ID}=="0010|0111|0112|0113|0114|0115|0116|0401|0402|0403|0404|0405|0406|0407|0410",\
+      RUN+="${systemd}/bin/loginctl lock-sessions"
+    '';
   };
-  yklock = "${yklock-pkg}/bin/yklock";
-in writeTextFile {
-  name = "yklock-udev-rules";
-  text = ''
-    ACTION=="remove", ENV{ID_VENDOR}=="Yubico", ENV{ID_VENDOR_ID}=="1050", ENV{ID_MODEL_ID}=="0010|0111|0112|0113|0114|0115|0116|0401|0402|0403|0404|0405|0406|0407|0410", RUN+="${yklock} lock"
-    ACTION=="add", ENV{ID_VENDOR}=="Yubico", ENV{ID_VENDOR_ID}=="1050", ENV{ID_MODEL_ID}=="0010|0111|0112|0113|0114|0115|0116|0401|0402|0403|0404|0405|0406|0407|0410", RUN+="${yklock} unlock"
-  '';
-  destination = "/etc/udev/rules.d/99-yklock.rules";
-}
+in
+  stdenv.mkDerivation rec {
+    pname = "yklock-udev-rules";
+    name = pname;
+    dontUnpack = true;
+    installPhase = ''
+      install -Dm 644 "${udevRules}" "$out/lib/udev/rules.d/99-yklock.rules"
+    '';
+  }
