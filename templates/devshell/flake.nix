848f746cdf638373c77957946d976f4ddf468d7c
mihranmashhud <mihranmashhud@gmail.com>
2024-10-13 16:46:29 -0400

    Update devshell template

--- templates/devshell/flake.nix
+++ templates/devshell/flake.nix
@@ -1,22 +1,52 @@
 {
-  description = "Dev shell provided via a flake.";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
 
   outputs = {
+    self,
     nixpkgs,
-    flake-utils,
+    devenv,
+    systems,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          # Add packages here as required.
-        ];
-      };
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
     });
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              packages = with pkgs; [
+                # Add packages here (https://devenv.sh/packages/)
+              ];
+              languages = {
+                # Enable languages here (https://devenv.sh/languages/)
+              };
+              enterShell =
+                # bash
+                ''
+                '';
+              # Add extra settings here (https://devenv.sh/basics/)
+            }
+          ];
+        };
+      });
+  };
 }
