3b45527c147962f6f09cbf74c3c37ed5e3a900ee
mihranmashhud <mihranmashhud@gmail.com>
2025-08-04 15:13:17 -0400

    Gitsigns keymaps.

--- modules/home/cli/nixvim/plugins/gitsigns.nix
+++ modules/home/cli/nixvim/plugins/gitsigns.nix
@@ -17,5 +17,34 @@ with lib.${namespace}; {
     plugins.gitsigns = {
       enable = true;
     };
+    extraConfigLua = ''
+      set_group_name("<leader>g", "Git")
+    '';
+    keymaps = [
+      {
+        mode = ["n" "v"];
+        key = "<leader>gu";
+        action = "<cmd>Gitsigns reset_hunk<cr>";
+        options = {
+          desc = "reset hunk";
+        };
+      }
+      {
+        mode = ["n" "v"];
+        key = "<leader>gp";
+        action = "<cmd>Gitsigns preview_hunk<cr>";
+        options = {
+          desc = "preview hunk";
+        };
+      }
+      {
+        mode = ["n" "v"];
+        key = "<leader>gb";
+        action = "<cmd>Gitsigns blame_line<cr>";
+        options = {
+          desc = "blame line";
+        };
+      }
+    ];
   };
 }
