cdd75219a2f4619135f69d5c6627919adc7a7008
mihranmashhud <mihranmashhud@gmail.com>
2025-08-24 08:57:01 -0400

    Add toggleterm.

---
+++ modules/home/cli/nixvim/plugins/toggleterm.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  pkgs,
+  inputs,
+  namespace, # The namespace used for your flake, defaulting to "internal" if not set.
+  system, # The system architecture for this host (eg. `x86_64-linux`).
+  target, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+  format, # A normalized name for the system target (eg. `iso`).
+  virtual, # A boolean to determine whether this system is a virtual target using nixos-generators.
+  systems, # An attribute map of your defined hosts.
+  config,
+  ...
+}:
+with lib;
+with lib.${namespace}; {
+  config.programs.nixvim = {
+    plugins.toggleterm = {
+      enable = true;
+      settings = {
+        direction = "float";
+        open_mapping = "[[<leader>t]]";
+        shade_terminals = false;
+        float_opts.border = config.programs.nixvim.opts.winborder;
+        insert_mappings = false;
+      };
+    };
+  };
+}
